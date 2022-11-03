2c6f5778934ac042dabc8495b58ff1a271f917ea
Arthur Guo <saturnmfgg@gmail.com>
2022-11-03 18:45:03 -0500

    Removed secret

--- src/api/yelp.ts
+++ src/api/yelp.ts
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer oGRBbJXjTtEkIOFiODJ9YDiYyLP07bSFJrfYBrpeczHhCkG5SBsA0KZV8KJfLmZoKh3JS4KFZRY2bo8Aswpc3SphVsOgWFCkLPeOVqxI_EScWptHhUjzBFIcOFFYY3Yx",
+      "Bearer ",
   },
 });
